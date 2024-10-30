f0debf19c652388feaf8f0ee330bde9e247d17e2
AshDL1994 <ashleydevereuxlewis@gmail.com>
2024-10-30 14:13:14 +0000

    adjusted test file with sys

--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,13 @@
 from src.main import mac_welcome
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 
 def test_not_on_mac():
     not_mac = mac_welcome()
-    assert not_mac != "You are NOT on a Mac!"
+    assert not_mac == "You are NOT on a Mac!"
 
 def test_on_mac():
     not_mac = mac_welcome()
