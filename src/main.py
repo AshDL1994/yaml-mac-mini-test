26af2e6a6b7d0b93b15a276aaaa4fc9633b0b243
AshDL1994 <ashleydevereuxlewis@gmail.com>
2024-10-30 11:44:56 +0000

    adding the required folders and files

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import platform
+
+def mac_welcome():
+
+    mac_type = str(platform.mac_ver())
+    mac_system = platform.system()
+
+    if mac_type != "('', ('', '', ''), '')" and mac_system == "Darwin":
+        return "You are on a Mac!"
+    
+    return "You are NOT on a Mac!"
+
+print(mac_welcome())
---
+++ test/test_main.py
@@ -0,0 +1,9 @@
+from src.main import mac_welcome
+
+def test_not_on_mac():
+    not_mac = mac_welcome()
+    assert not_mac == "You are NOT on a Mac!"
+
+def test_on_mac():
+    not_mac = mac_welcome()
+    assert not_mac == "You are on a Mac!"    
