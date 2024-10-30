ed892570284c41dca1e49041a145c543509eb963
AshDL1994 <ashleydevereuxlewis@gmail.com>
2024-10-30 14:07:50 +0000

    test file adjusted

--- test/test_main.py
+++ test/test_main.py
@@ -2,7 +2,7 @@ from src.main import mac_welcome
 
 def test_not_on_mac():
     not_mac = mac_welcome()
-    assert not_mac == "You are NOT on a Mac!"
+    assert not_mac != "You are NOT on a Mac!"
 
 def test_on_mac():
     not_mac = mac_welcome()
